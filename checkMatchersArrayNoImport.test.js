b750aba8ea87c0b8d649f6f763c9a5f70fbd8d78
William Daly <will.daly.91@gmail.com>
2022-04-25 11:27:02 +0100

    Adding read me and some new examples

--- checkMatchersArrayNoImport.test.js
+++ checkMatchersArrayNoImport.test.js
@@ -10,3 +10,12 @@ test('The shopping list has milk', () => {
     expect(shoppingList).toContain('milk');
     expect(new Set(shoppingList)).toContain('milk');
 });
+
+
+test('A cheat sheet with some other small examples', () => {
+  expect([]).toEqual(expect.any(Array))
+  expect(['Alice', 'Bob', 'Eve']).toHaveLength(3)
+  expect(['Alice', 'Bob', 'Eve']).toContain('Alice')
+  expect([{ a: 1 }, { a: 2 }]).toContainEqual({ a: 1 })
+  expect(['Alice', 'Bob', 'Eve']).toEqual(expect.arrayContaining(['Alice', 'Bob']))
+});
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
+Great and simple place to start - https://jestjs.io/docs/getting-started
+
 https://simplecheatsheet.com/tag/jest-cheat-sheet/
 
 https://jestjs.io/docs/asynchronous
+
+
