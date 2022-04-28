f55e3ee9106d3eb8b0340bc9fdad3c0518bf5aba
William Daly <will.daly.91@gmail.com>
2022-04-28 10:41:55 +0100

    Adding hooks and regex

--- soccer.test.js
+++ soccer.test.js
@@ -12,65 +12,89 @@ const x = require("./soccer");
 //     "AwayTeamScore": 1
 // },
 
-test("Test : Check the array properties 🍍🍍🍍", () => 
-{
-    expect(x).not.toBeNull();
-    expect(x).toBeDefined();
-    expect(x).not.toBeUndefined();
-    expect(x).toBeTruthy(); // (not false, 0, ”, null, undefined, NaN)
-    expect(x).not.toBeFalsy();
-    expect(x).toEqual(expect.any(Array));
-    expect(typeof x).toBe("object");
-    expect(x).toHaveLength(380);
-});
+describe('makePoniesPink', () => {
+    beforeAll(() => {
+        console.log("This is the beforeAll() block 🍉 🍉 🍉 🍉");
+        console.log("Runs before all tests");
+    })
+    afterAll(() => {
+        console.log("This is the afterAll() block 🍍 🍍 🍍 🍍");
+        console.log("Runs after all tests");
+    })
+    beforeEach(() => {
+        console.log("This is the beforeEach() block 🫐 🫐 🫐 🫐");
+        console.log("Runs before each test");
+    })
+    afterEach(() => {
+        console.log("This is the afterEach() block 🥥 🥥 🥥 🥥");
+        console.log("Runs after each test");
+    })
 
-test("Test : Interrogate properties of this JSON feed ⚽ ⚽ ⚽ ", () => 
-{
-    for(let [k, v] of Object.entries(x))
+    test("Test [1] : Check the array properties!", () => 
     {
-        for(let [k1, v1] of Object.entries(v))
+        expect(x).not.toBeNull();
+        expect(x).toBeDefined();
+        expect(x).not.toBeUndefined();
+        expect(x).toBeTruthy(); // (not false, 0, ”, null, undefined, NaN)
+        expect(x).not.toBeFalsy();
+        expect(x).toEqual(expect.any(Array));
+        expect(typeof x).toBe("object");
+        expect(x).toHaveLength(380);
+    });
+
+    test("Test [2] : Interrogate properties of this JSON feed!", () => 
+    {
+        for(let [k, v] of Object.entries(x))
         {
-            if(k1 == "HomeTeam" || k1 == "AwayTeam" || k1 == "Location")
+            for(let [k1, v1] of Object.entries(v))
             {
-                expect(v1).not.toBeUndefined();
-                expect(v1).toBeDefined();
-                expect(v1).toBeTruthy(); 
-                expect(typeof v1).toBe("string"); // .toBe method tests for exact (===) equality
-                expect(v1).not.toMatch("Derby");
-            }
-            else if((k1 == "HomeTeamScore" && v1 != null) || (k1 == "AwayTeamScore" && v1 != null))
-            {
-                expect(v1).toEqual(expect.any(Number))
-                expect(typeof v1).toBe("number"); 
-                expect(v1).toBeGreaterThan(-1)
-                expect(v1).toBeGreaterThanOrEqual(0);
-                expect(v1).toBeLessThan(39);
-                expect(v1).toBeLessThanOrEqual(38)
-                expect(0.2 + 0.1).toBeCloseTo(0.3, 5) // Still need to understand this better 
-            }
-            else if(k1 == "RoundNumber")
-            {
-                expect(v1).toBeTruthy();
-                expect(typeof v1).toBe("number");
-                expect(v1).toBeGreaterThanOrEqual(1);
-                expect(v1).toBeLessThan(39);
-            }
-            else if(k1 == "MatchNumber")
-            {
-                expect(v1).toBeTruthy();
-                expect(typeof v1).toBe("number");
-                expect(v1).toBeGreaterThanOrEqual(0);
-                expect(v1).toBeLessThan(381);
-            }
-            else if(k1 == "Group")
-            {
-                expect(v1).toBeFalsy();
-                expect(v1).toBeNull();
-            }
-            else if(k1 == "DateUtc")
-            {
-                expect(v1).toMatch(/\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2}:\d{2}/);
+                if(k1 == "HomeTeam" || k1 == "AwayTeam" || k1 == "Location")
+                {
+                    expect(v1).not.toBeUndefined();
+                    expect(v1).toBeDefined();
+                    expect(v1).toBeTruthy(); 
+                    expect(typeof v1).toBe("string"); // .toBe method tests for exact (===) equality
+                    expect(v1).not.toMatch("Derby");
+                }
+                else if((k1 == "HomeTeamScore" && v1 != null) || (k1 == "AwayTeamScore" && v1 != null))
+                {
+                    expect(v1).toEqual(expect.any(Number))
+                    expect(typeof v1).toBe("number"); 
+                    expect(v1).toBeGreaterThan(-1)
+                    expect(v1).toBeGreaterThanOrEqual(0);
+                    expect(v1).toBeLessThan(39);
+                    expect(v1).toBeLessThanOrEqual(38)
+                    expect(0.2 + 0.1).toBeCloseTo(0.3, 5) // Still need to understand this better 
+                }
+                else if(k1 == "RoundNumber")
+                {
+                    expect(v1).toBeTruthy();
+                    expect(typeof v1).toBe("number");
+                    expect(v1).toBeGreaterThanOrEqual(1);
+                    expect(v1).toBeLessThan(39);
+                }
+                else if(k1 == "MatchNumber")
+                {
+                    expect(v1).toBeTruthy();
+                    expect(typeof v1).toBe("number");
+                    expect(v1).toBeGreaterThanOrEqual(0);
+                    expect(v1).toBeLessThan(381);
+                }
+                else if(k1 == "Group")
+                {
+                    expect(v1).toBeFalsy();
+                    expect(v1).toBeNull();
+                }
+                else if(k1 == "DateUtc")
+                {
+                    expect(v1).toMatch(/\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2}:\d{2}/);
+                }
             }
         }
-    }
+    })
+
 })
+
+
+
+
